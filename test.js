38f52326c260669bc4eb3824e77c603b87e6745e
amekusa <hello@amekusa.com>
2020-06-28 22:06:56 +0900

    + test command

---
+++ test.js
@@ -0,0 +1,30 @@
+const flexParams = require('./index.js');
+
+class User {
+  constructor(...args) {
+    flexParams(args, this, [
+      // patterns
+      { firstName:'string', age:'int' },
+      { firstName:'string', lastName:'string', age:'int' },
+      { id:'int' },
+      { login:'string', pass:Password }
+    ]);
+  }
+}
+
+class Password {
+  constructor(key) {
+    this.key = key;
+  }
+}
+
+//// Test ////////////
+let thomas = new User('Thomas', 20);
+let john   = new User('John', 'Doe', 30);
+let user1  = new User(1000);
+let user2  = new User('d4rk10rd', new Password('asdf'));
+
+console.log(thomas);
+console.log(john);
+console.log(user1);
+console.log(user2);
